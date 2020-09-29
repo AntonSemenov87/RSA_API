56c33342db81e5be60e8a796be7f819048aa6ff3
AntonSemenov87 <Anthony04061987@gmail.com>
2020-09-28 23:09:17 -0500

    updates

---
+++ src/test/java/ComplexJsonPath.java
@@ -0,0 +1,47 @@
+
+import files.Payload;
+import io.restassured.path.json.JsonPath;
+
+import java.util.Iterator;
+
+public class ComplexJsonPath {
+    public static void main(String[] args) {
+
+        JsonPath js = new JsonPath(Payload.coursePrice());
+
+       // 1. Print No of courses returned by API
+        int count = js.getInt("courses.size()");
+        System.out.println("Courses number: " + count);
+
+        //2.Print Purchase Amount
+        int purchaseAmount = js.getInt("dashboard.purchaseAmount");
+        System.out.println("Total purchase amount: " + purchaseAmount);
+
+       // 3. Print Title of the first course
+        String firstCourseTitle = js.getString("courses[0].title");
+        System.out.println(firstCourseTitle);
+
+       // 4. Print All course titles and their respective Prices
+        for (int i = 0; i < count; i++) {
+            String courseTitles = js.get("courses[" + i + "].title");
+            int price = js.get("courses[" + i + "].price");
+            System.out.println(courseTitles + " " + price);
+        }
+
+
+      //  5. Print no of copies sold by RPA Course
+        int numOfRPACopies = js.getInt("courses[2].copies");
+        System.out.println(numOfRPACopies);
+
+       // 6. Verify if Sum of all Course prices matches with Purchase Amount
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            int price = js.get("courses[" + i + "].price");
+            int quantity = js.getInt("courses[" + i + "].copies");
+            sum += price * quantity;
+        }
+        System.out.println("Total amount of these books is: " + sum);
+
+
+    }
+}
